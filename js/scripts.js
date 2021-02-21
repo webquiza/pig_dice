16b3dd5d55465a59a78d6b06204ded6d22755ef1
Carlos Urquiza <webquiza@gmail.com>
2021-02-21 13:43:47 -0800

    add html buttons and initial js code
    Co-authored-by: Parker Benoit <parker.benoit44@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
 // Business logic 
 
+//Score.prototype.scoreAdd = function (currentPlayer, newScore, scoreTotal) {
+  //if currentPlayer 
+//}
+  
+//}
 
+function randomNumber () {
+  Math.floor(Math.random() * 6) + 1;
+}
 
-
-
+[currentPlayer , standbyPlayer] = ["player1", "player2"];
 // User Interface Logic 
+
+$(document).ready(function() {
+  $("form#rollDice").submit(function(event) {
+    event.preventDefault();
+    let randomNumber1 = randomNumber();
+    $(".newScore").html(randomNumber1);
+    
+  })
+})
